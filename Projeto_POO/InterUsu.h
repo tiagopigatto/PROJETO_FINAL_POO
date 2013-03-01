4a27bf4607ffef45a8b8677b603708407ddaaaf2
Tiago Pigatto Lenza <tiago.sk82@gmail.com>
2013-02-28 23:45:46 -0300

    COMMIT - HEADER InterUsu.h
    Começo da declaração das classes

--- Projeto_POO/InterUsu.h
+++ Projeto_POO/InterUsu.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   InterfaceUsuario.h
  * Author: Vitor
  *
@@ -9,22 +9,60 @@
 #define	INTERFACEUSUARIO_H
 
 class BasicScreen {
-private:
-    virtual void showDetails();
-    virtual void showOptions();
-
+protected:
+    virtual void showScreen() = 0;
+    virtual void showOptions() = 0;
 public:
     BasicScreen(User user);
+    virtual void telaExec();
     void exec();
 };
 
 class StartPage : public BasicScreen {
+private:
+    static const unsigned int N_OPCOES = 5
+    static const unsigned int N_LOGIN = 1
+    static const unsigned int N_CADASTRO = 2
+    static const unsigned int N_LIU = 3
+    static const unsigned int N_LIP = 4
+    static const unsigned int N_LP = 5
+
+public:
+void showScreen();
+
 };
 
-class PostPage : public BasicScreen {
+class PostPageAut : public BasicScreen {
+private:
+    static const unsigned int N_OPCOES = 2
+    static const unsigned int N_APOST = 1
+    static const unsigned int N_SUBCOMENT = 2
+
+public:
+void showScreen();
 };
 
-class ComentPage : public BasicScreen {
+class PostPageNAut : public BasicScreen {
+private:
+    static const unsigned int N_OPCOES = 2
+    static const unsigned int N_MOSTEXTO = 1
+    static const unsigned int N_SUBCOMENT = 2
+
+public:
+void showScreen();
+};
+
+class MenuLogin : public BasicScreen {
+private:
+    static const unsigned int N_OPCOES = 5
+    static const unsigned int N_ALTERAR = 1
+    static const unsigned int N_ENCERRAR = 2
+    static const unsigned int N_POSTAR = 3
+    static const unsigned int N_LISTAR_POST = 4
+    static const unsigned int N_LISTAR_IDENT = 5
+
+public:
+void showScreen();
 };
 
 
