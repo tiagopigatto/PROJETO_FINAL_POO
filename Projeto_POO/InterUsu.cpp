e8d100c5f42587ba9fb94201ada5431b0745f065
vitorecomp <vitor.ecomp@gmail.com>
2013-03-01 02:28:20 -0300

    Pigato deixei pronto pra implemetar

--- Projeto_POO/InterUsu.cpp
+++ Projeto_POO/InterUsu.cpp
@@ -1,1 +1,33 @@
 
+#include "InterUsu.h"
+
+void StartPage::showScreen(){
+}
+
+void StartPage::verOptions(){
+    
+}
+
+void PostPageAut::showScreen(){
+    
+}
+
+void PostPageAut::verOptions(){
+    
+}
+
+void PostPageNAut::showScreen(){
+    
+}
+
+void PostPageNAut::showScreen(){
+    
+}
+
+void MenuLogin::showScreen(){
+    
+}
+
+void MenuLogin::verOptions(){
+
+}
--- Projeto_POO/InterUsu.h
+++ Projeto_POO/InterUsu.h
@@ -20,53 +20,53 @@ public:
 
 class StartPage : public BasicScreen {
 private:
-    static const unsigned int N_OPCOES = 5
-    static const unsigned int N_LOGIN = 1
-    static const unsigned int N_CADASTRO = 2
-    static const unsigned int N_LIU = 3
-    static const unsigned int N_LIP = 4
-    static const unsigned int N_LP = 5
+    static const unsigned int N_OPCOES = 5;
+    static const unsigned int N_LOGIN = 1;
+    static const unsigned int N_CADASTRO = 2;
+    static const unsigned int N_LIU = 3;
+    static const unsigned int N_LIP = 4;
+    static const unsigned int N_LP = 5;
 
 public:
-void showScreen();
-void verOptions();
+    void showScreen();
+    void verOptions();
 
 };
 
 class PostPageAut : public BasicScreen {
 private:
-    static const unsigned int N_OPCOES = 2
-    static const unsigned int N_APOST = 1
-    static const unsigned int N_SUBCOMENT = 2
+    static const unsigned int N_OPCOES = 2;
+    static const unsigned int N_APOST = 1;
+    static const unsigned int N_SUBCOMENT = 2;
 
 public:
-void showScreen();
-void verOptions();
+    void showScreen();
+    void verOptions();
 };
 
 class PostPageNAut : public BasicScreen {
 private:
-    static const unsigned int N_OPCOES = 2
-    static const unsigned int N_MOSTEXTO = 1
-    static const unsigned int N_SUBCOMENT = 2
+    static const unsigned int N_OPCOES = 2;
+    static const unsigned int N_MOSTEXTO = 1;
+    static const unsigned int N_SUBCOMENT = 2;
 
 public:
-void showScreen();
-void verOptions();
+    void showScreen();
+    void verOptions();
 };
 
 class MenuLogin : public BasicScreen {
 private:
-    static const unsigned int N_OPCOES = 5
-    static const unsigned int N_ALTERAR = 1
-    static const unsigned int N_ENCERRAR = 2
-    static const unsigned int N_POSTAR = 3
-    static const unsigned int N_LISTAR_POST = 4
-    static const unsigned int N_LISTAR_IDENT = 5
+    static const unsigned int N_OPCOES = 5;
+    static const unsigned int N_ALTERAR = 1;
+    static const unsigned int N_ENCERRAR = 2;
+    static const unsigned int N_POSTAR = 3;
+    static const unsigned int N_LISTAR_POST = 4;
+    static const unsigned int N_LISTAR_IDENT = 5;
 
 public:
-void showScreen();
-void verOptions();
+    void showScreen();
+    void verOptions();
 };
 
 
