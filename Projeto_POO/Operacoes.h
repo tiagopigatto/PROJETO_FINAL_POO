87f376d2c97531314f0d9e3044cd57cec002225c
fraps <matrpedreira@gmail.com>
2013-03-01 23:39:17 -0300

    Erro após comentários DOXYGEN

--- Projeto_POO/Operacoes.h
+++ Projeto_POO/Operacoes.h
@@ -1,6 +1,6 @@
 ﻿/**
  * \file   Operacoes.h
- * \brief  Este e o cabecalho resposável por gerenciar os comandos SQL nas tabelas: 
+ * \brief  Este e o cabecalho resposável por gerenciar os comandos BD nas tabelas: 
  * \n Os comandos são na ordem da classe:\n
  * Criar um novo usário\n
  * Atualizar um usuário\n
@@ -16,10 +16,10 @@
  * Deletar um post\n
  * Procurar um id na tabela post\n
  */
-
 #include "IReciever.h"
 #include "Entidades.h"
 #include "sqlite3.h"
+//e ai queo q será? isso apareceu dps que comecei a comentar
 
 class Operacoes : public IReciever {
     User user_;
@@ -30,10 +30,11 @@ class Operacoes : public IReciever {
 public:
 
     //! Construtor de entidades
+
     /*!
      Podem ser geradas usadas as entidades user, coment ou post para realizar
      * operções no banco de dados
-    */
+     */
 
     Operacoes(User user)
     : user_(user) {
@@ -50,7 +51,12 @@ public:
     void SetAction(TYPES::ACTION_LIST action) {
         currentAction = action;
     }
-    //Ações
+    //! Recebe os resultados
+
+    /*!
+     Construtor que gerencia as operações em sql e também o retorno dessas operações.
+     */
+
 
     int GetResult() {
 
@@ -69,6 +75,13 @@ public:
             /** Erro no banco **/
         }
         int result;
+
+        /*! \brief Cadastra Usuário No BD
+         *
+         *
+         * Recebe o objeto User e apartir dele é gerado uma linha no BD, no entanto isso só ocorre caso\n
+         * não exista nem um indentificador nem um nome igual na tabela
+         */
         if (currentAction == TYPES::ACTION_LIST::CADASTRAUSER) {
             //Sem verificação de existência do nome no BD
             //ADD a entidade no BD
@@ -85,7 +98,14 @@ public:
             if (retval) {
                 /** ID ou NOME já existe **/
             }
-        } else if (currentAction == TYPES::ACTION_LIST::UPDATEUSER) {
+        }
+            /*! \brief Atualiza Usuário No BD
+             *
+             *
+             * Recebe o objeto User e apartir dele são atualizados o nome e a senha, se existir um identificador e o novo nome não
+             * existir ainda.
+             */
+        else if (currentAction == TYPES::ACTION_LIST::UPDATEUSER) {
             //Sem verificação de existência do ID no BD
             //Update na entidade no BD
             sql = (char *) malloc(sizeof (char) * q_size);
@@ -101,14 +121,25 @@ public:
             if (retval) {
                 /** NOME já existe **/
             }
-        } else if (currentAction == TYPES::ACTION_LIST::DELETEUSER) {
+        }            /*! \brief Deleta Usuário No BD
+        *
+        *
+        * Recebe o objeto endereço que deve ser deletado e apartir dele faz um drop Delete na linha 
+        */
+        else if (currentAction == TYPES::ACTION_LIST::DELETEUSER) {
             //Delete na entidade no BD            
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "DELETE FROM user WHERE Identify = '");
             strcat(sql, user_.getIdentify());
             strcat(sql, "'");
             retval = sqlite3_exec(handle, sql, 0, 0, 0);
-        } else if (currentAction == TYPES::ACTION_LIST::FINDUSERN) {
+        }
+            /*! \brief Procura um nome de Usuário No BD
+             *
+             *
+             * Recebe um Name e procura na tabela se esse existe, caso sim armazena as informações dele em um objeto User 
+             */
+        else if (currentAction == TYPES::ACTION_LIST::FINDUSERN) {
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "SELECT * FROM user WHERE name = '");
             strcat(sql, user_.getName());
@@ -136,7 +167,12 @@ public:
                     break;
                 }
             }
-        } else if (currentAction == TYPES::ACTION_LIST::FINDUSERID) {
+        }            /*! \brief Procura um id de Usuário No BD
+        *
+        *
+        * Recebe um identificador e procura na tabela se esse existe, caso sim armazena as informações dele em um objeto User 
+        */
+        else if (currentAction == TYPES::ACTION_LIST::FINDUSERID) {
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "SELECT * FROM user WHERE Identify = '");
             strcat(sql, user_.getIdentify());
@@ -164,7 +200,14 @@ public:
                     break;
                 }
             }
-        } else if (currentAction == TYPES::ACTION_LIST::CADASTRACOMENT) {
+        }
+            /*! \brief Cadastra Comentário No BD
+             *
+             *
+             * Recebe o objeto Coment e apartir dele é gerado uma linha no BD, no entanto isso só ocorre caso\n
+             * não exista o identificador do comentário na tabela
+             */
+        else if (currentAction == TYPES::ACTION_LIST::CADASTRACOMENT) {
             //ADD a entidade no BD
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "INSERT INTO coment VALUES('");
@@ -183,7 +226,14 @@ public:
             if (retval) {
                 /** ID já existe **/
             }
-        } else if (currentAction == TYPES::ACTION_LIST::UPDATECOMENT) {
+        }
+            /*! \brief Atualiza Comentário No BD
+             *
+             *
+             * Recebe o objeto Coment e apartir dele é gerado uma linha no BD, no entanto isso só ocorre caso\n
+             * não exista o identificador do comentário na tabela
+             */
+        else if (currentAction == TYPES::ACTION_LIST::UPDATECOMENT) {
             //Update na entidade no BD
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "UPDATE coment SET comentText = '");
@@ -198,14 +248,26 @@ public:
             if (retval) {
                 /** BUG **/
             }
-        } else if (currentAction == TYPES::ACTION_LIST::DELETECOMENT) {
+        }
+            /*! \brief Deleta Comentário No BD
+             *
+             *
+             * Recebe o id de um Comentário e apartir dele é feito um delete na linha caso essa exista.
+             */
+        else if (currentAction == TYPES::ACTION_LIST::DELETECOMENT) {
             //Delete na entidade no BD            
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "DELETE FROM coment WHERE comentIdentify = '");
             strcat(sql, coment_.getComentIdentify());
             strcat(sql, "'");
-            retval = sqlite3_exec(handle, sql, 0, 0, 0); 
-        } else if (currentAction == TYPES::ACTION_LIST::FINDCOMENT) {
+            retval = sqlite3_exec(handle, sql, 0, 0, 0);
+        }
+            /*! \brief Procura um id de Comentário No BD
+             *
+             *
+             * Recebe o id de busca e caso encontre coloca os valores da linha em um objeto Coment
+             */
+        else if (currentAction == TYPES::ACTION_LIST::FINDCOMENT) {
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "SELECT * FROM coment WHERE comentIdentify = '");
             strcat(sql, coment_.getComentIdentify());
@@ -237,7 +299,13 @@ public:
                     break;
                 }
             }
-        } else if (currentAction == TYPES::ACTION_LIST::CADASTRAPOST) {
+        }
+            /*! \brief Cadastra um Post No BD
+             *
+             *
+             * Recebe o objeto Post e armazena no Banco de Dados, só armazena se o id do Post não existir
+             */
+        else if (currentAction == TYPES::ACTION_LIST::CADASTRAPOST) {
             //ADD a entidade no BD
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "INSERT INTO post VALUES('");
@@ -248,13 +316,19 @@ public:
             strcat(sql, post_.getDate());
             strcat(sql, "',NULL,'");
             strcat(sql, post_.getAuthorIdentify());
-            strcat(sql, "')");
+            strcat(sql, "',0)");
             // executa nosso comando no banco 
             retval = sqlite3_exec(handle, sql, 0, 0, 0);
             if (retval) {
                 /** ID já existe **/
             }
-        } else if (currentAction == TYPES::ACTION_LIST::UPDATEPOST) {
+        }
+            /*! \brief Atualiza um Post No BD
+             *
+             *
+             * Recebe o id de um Post e atualiza seu texto, data e avaliação se esse post existir
+             */
+        else if (currentAction == TYPES::ACTION_LIST::UPDATEPOST) {
             //Update na entidade no BD
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "UPDATE post SET postText = '");
@@ -269,14 +343,26 @@ public:
             if (retval) {
                 /** Atualizar ID q n existe **/
             }
-        } else if (currentAction == TYPES::ACTION_LIST::DELETEPOST) {
+        }
+            /*! \brief Deleta um Post No BD
+             *
+             *
+             * Recebe o id de um Post e deleta a linha em que ele se encontra
+             */
+        else if (currentAction == TYPES::ACTION_LIST::DELETEPOST) {
             //Delete na entidade no BD            
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "DELETE FROM post WHERE postIdentify = '");
             strcat(sql, post_.getPostIdentify());
             strcat(sql, "'");
-            retval = sqlite3_exec(handle, sql, 0, 0, 0); 
-        } else if (currentAction == TYPES::ACTION_LIST::FINDPOST) {
+            retval = sqlite3_exec(handle, sql, 0, 0, 0);
+        }
+            /*! \brief Encontra um Post pelo id No BD
+             *
+             *
+             * Recebe o id de um Post e o armazena no objeto Post
+             */
+        else if (currentAction == TYPES::ACTION_LIST::FINDPOST) {
             sql = (char *) malloc(sizeof (char) * q_size);
             strcpy(sql, "SELECT * FROM post WHERE postIdentify = '");
             strcat(sql, post_.getPostIdentify());
