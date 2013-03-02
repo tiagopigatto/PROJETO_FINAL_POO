ce8140a644badcb2a01aea5ca50de2ee7b1be146
vitorecomp <vitor.ecomp@gmail.com>
2013-03-01 21:23:01 -0300

    FOi

--- Projeto_POO/Commands.h
+++ Projeto_POO/Commands.h
@@ -1,225 +1,203 @@
 #pragma once
 
-class ACommand
-{
+#include "IReciever.h"
+
+class ACommand {
 protected:
 
-	IReciever *pReciever_;
+    IReciever *pReciever_;
 
 public:
 
-	ACommand(IReciever *reciever)
-		:pReciever_(reciever)
-	{
-	}
+    ACommand(IReciever *reciever)
+    : pReciever_(reciever) {
+    }
 
-	virtual int Execute() = 0;
+    virtual int Execute() = 0;
 };
 
 //Usuário
-class CadastraUser : public ACommand
-{
+
+class CadastraUser : public ACommand {
 public:
-	CadastraUser(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    CadastraUser(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::CADASTRAUSER);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::CADASTRAUSER);
+        return pReciever_->GetResult();
+    }
 };
 
-class UpdateUser : public ACommand
-{
+class UpdateUser : public ACommand {
 public:
-	UpdateUser(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    UpdateUser(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::UPDATEUSER);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::UPDATEUSER);
+        return pReciever_->GetResult();
+    }
 };
 
-class DeleteUser : public ACommand
-{
+class DeleteUser : public ACommand {
 public:
-	DeleteUser(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    DeleteUser(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::DELETEUSER);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::DELETEUSER);
+        return pReciever_->GetResult();
+    }
 };
 
-class FindUserN : public ACommand
-{
+class FindUserN : public ACommand {
 public:
-	FindUserN(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    FindUserN(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::FINDUSERN);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::FINDUSERN);
+        return pReciever_->GetResult();
+    }
 };
 
-class FindUserID : public ACommand
-{
+class FindUserID : public ACommand {
 public:
-	FindUserID(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    FindUserID(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::FINDUSERID);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::FINDUSERID);
+        return pReciever_->GetResult();
+    }
 };
 //Comentários
 
-class CadastraComent : public ACommand
-{
+class CadastraComent : public ACommand {
 public:
-	CadastraComent(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    CadastraComent(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::CADASTRACOMENT);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::CADASTRACOMENT);
+        return pReciever_->GetResult();
+    }
 };
 
-class UpdateComent : public ACommand
-{
+class UpdateComent : public ACommand {
 public:
-	UpdateComent(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    UpdateComent(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::UPDATECOMENT);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::UPDATECOMENT);
+        return pReciever_->GetResult();
+    }
 };
 
-class DeleteComent : public ACommand
-{
+class DeleteComent : public ACommand {
 public:
-	DeleteComent(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    DeleteComent(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::DELETECOMENT);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::DELETECOMENT);
+        return pReciever_->GetResult();
+    }
 };
 
-class FindComent : public ACommand
-{
+class FindComent : public ACommand {
 public:
-	FindComent(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    FindComent(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::FINDCOMENT);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::FINDCOMENT);
+        return pReciever_->GetResult();
+    }
 };
 //POst
-class CadastraPost : public ACommand
-{
+
+class CadastraPost : public ACommand {
 public:
-	CadastraPost(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    CadastraPost(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::CADASTRAPOST);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::CADASTRAPOST);
+        return pReciever_->GetResult();
+    }
 };
 
-class UpdatePost : public ACommand
-{
+class UpdatePost : public ACommand {
 public:
-	UpdatePost(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    UpdatePost(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::UPDATEPOST);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::UPDATEPOST);
+        return pReciever_->GetResult();
+    }
 };
-class DeletePost : public ACommand
-{
+
+class DeletePost : public ACommand {
 public:
-	DeletePost(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    DeletePost(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::DELETEPOST);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::DELETEPOST);
+        return pReciever_->GetResult();
+    }
 };
-class FindPost : public ACommand
-{
+
+class FindPost : public ACommand {
 public:
-	FindPost(IReciever *reciever)
-		: ACommand(reciever)
-	{
 
-	}
+    FindPost(IReciever *reciever)
+    : ACommand(reciever) {
 
-	int Execute()
-	{
-		pReciever_->SetAction(TYPES::ACTION_LIST::FINDPOST);
-		return pReciever_->GetResult();
-	}
+    }
+
+    int Execute() {
+        pReciever_->SetAction(TYPES::ACTION_LIST::FINDPOST);
+        return pReciever_->GetResult();
+    }
 };
